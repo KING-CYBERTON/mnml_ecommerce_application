cf22a6e7d521d2182503a24090c4e8e597ddde78
mhmd <mohammadfallah.w@gmail.com>
2023-04-24 15:07:11 +0330

    image list view done

--- lib/screen/intro_screen.dart
+++ lib/screen/intro_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:mnml_ecommerce_application/util/colors.dart';
+import 'package:mnml_ecommerce_application/widget/image_list_view.dart';
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          // image transition
+          Positioned(
+            top: -10,
+            left: -150,
+            child: Row(
+              children: const <Widget>[
+                ImageListView(startIndex: 0),
+                ImageListView(startIndex: 1),
+                ImageListView(startIndex: 2),
+              ],
+            ),
+          ),
+
+          // title
+          Positioned(
+            top: MediaQuery.of(context).size.height * .8,
+            child: const Text(
+              'MNMLMANDI',
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
