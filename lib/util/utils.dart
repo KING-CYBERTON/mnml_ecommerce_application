9cdafd2b36f501b743cf2096cf1324fff1198388
mhmd <mohammadfallah.w@gmail.com>
2023-04-24 15:15:04 +0330

    init util file

---
+++ lib/util/utils.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final TextStyle kTitleStyle = GoogleFonts.ubuntu(
+  fontSize: 28,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 1.5,
+  color: Colors.white,
+);
